06e65e473ecbc8d7a18ccd17dd4e5bcfc301d2dd
uiratan <uiratan@gmail.com>
2023-03-09 19:37:41 -0300

    Listagem e criação de pensamento pelo consumo de API do backend

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
+import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,23 +11,26 @@ import { Component, OnInit } from '@angular/core';
 export class CriarPensamentoComponent implements OnInit {
 
   pensamento: Pensamento = {
-    id: 1,
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
+    conteudo: '',
+    autoria: '',
     modelo: 'modelo1'
   }
 
-  constructor() { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
   criarPensamento() {
-    alert("Novo pensamento criado");
+    this.service.criar(this.pensamento).subscribe();
+    this.router.navigate(['/listarPensamento']);
   }
 
   cancelar() {
-    // alert("Pensamento cancelado");
+    this.router.navigate(['/listarPensamento']);
   }
 
 }
